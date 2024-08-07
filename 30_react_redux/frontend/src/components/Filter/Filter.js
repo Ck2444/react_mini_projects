afb0ac0ac49f8a8d55f5d9b05b1feecdcf192f55
Konstantin Evstigneev <t777tt88@gmail.com>
2024-08-07 09:42:30 +0200

    Added BookForm, BookLIst and Filter components

---
+++ 30_react_redux/frontend/src/components/Filter/Filter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Filter.css';
+
+export default function Filter() {
+  return (
+    <div className="app-block filter">
+      <h2>Filters</h2>
+    </div>
+  );
+}
